01494ff030642e7f09cf580fa8ad538a3dc33ef6
Praj2003 <prajval.kanda01@gmail.com>
2025-07-25 11:12:10 +0530

    third commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    // Apply to all TS/JS files
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      // Disable annoying rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "warn", // Or "off" if needed
+    },
+  },
 ];
 
 export default eslintConfig;
