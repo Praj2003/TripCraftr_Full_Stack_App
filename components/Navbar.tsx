a718e096584cdab7a0f0ee4967ba5de02bc10d3a
Praj2003 <prajval.kanda01@gmail.com>
2025-07-29 15:55:50 +0530

    Added a dropdown to hamburger symbol (for small screens)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion } from "motion/react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 import {
   SignInButton,
   SignOutButton,
@@ -13,6 +14,7 @@ import {
 } from "@clerk/nextjs";
 
 const Navbar = () => {
+  const [openDropDown, setOpenDropDown] = useState<boolean | undefined>(false);
   return (
     <div className="min-w-full fixed top-0 flex justify-between items-center z-20 bg-black">
       <div className="logo flex items-center justify-center">
@@ -65,19 +67,60 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center justify-between mr-5">
-        <div className="md:hidden lg:hidden">
-          <Link href="/menu">
+        <div
+          className="md:hidden lg:hidden relative"
+          onMouseEnter={() => setOpenDropDown(true)}
+          onMouseLeave={() => setOpenDropDown(false)}
+        >
+          <Link href="/">
             <motion.div
+              onClick={() => setOpenDropDown(!openDropDown)}
               whileHover={{
                 scale: 1.1,
-                backgroundColor: "white",
-                color: "black",
               }}
               className="px-3 py-2 text-white rounded-lg font-bold"
             >
               <GiHamburgerMenu size={30} />
             </motion.div>
           </Link>
+          {openDropDown && (
+            <div className="absolute w-[250px] top-8 right-0 bg-black text-white font-bold p-2 rounded-lg">
+              <div className="flex flex-col items-center gap-5">
+                <motion.div
+                  whileHover={{
+                    scale: 1.1,
+                    backgroundColor: "white",
+                    color: "black",
+                  }}
+                  className="w-full text-center cursor-pointer rounded-lg px-3 py-1"
+                >
+                  <Link href={"/"}>Home</Link>
+                </motion.div>
+
+                <motion.div
+                  whileHover={{
+                    scale: 1.1,
+                    backgroundColor: "white",
+                    color: "black",
+                  }}
+                  className="w-full text-center cursor-pointer rounded-lg px-3 py-1"
+                >
+                  <Link href={"/ai"}>Get Started</Link>
+                </motion.div>
+
+                <motion.div
+                  whileHover={{
+                    scale: 1.1,
+                    backgroundColor: "white",
+                    color: "black",
+                  }}
+                  className="w-full text-center cursor-pointer rounded-lg px-3 py-1"
+                >
+                  <Link href={"/subscribe"}>Subscribe</Link>
+                </motion.div>
+              </div>
+            </div>
+          )}
         </div>
         <SignedOut>
           <div className="flex items-center justify-center">
